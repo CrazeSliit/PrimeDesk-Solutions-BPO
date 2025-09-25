3518b35a285a6032d54bc9b88c4fe0760a6c881c
Tharindu Chamuditha <159540325+CrazeSliit@users.noreply.github.com>
2025-09-25 07:13:41 +0530

    fix: update LinkedIn link in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export const Footer = () => {
             </p>
             <div className="flex space-x-4">
               {/* Social Media Icons */}
-              <a href="#" className="text-gray-300 hover:text-[#FF6600]">
+              <a href="https://www.linkedin.com/company/prime-desk-solutions/" className="text-gray-300 hover:text-[#FF6600]">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin">
                   <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
                   <rect width="4" height="12" x="2" y="9" />
